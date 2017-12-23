0c68b85c3968e00bbaea80a2b6e871d3c550781c
hworthX51@hotmail.com <you@example.com>
2017-12-23 13:55:21 -0500

    added friend info

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+//all your possible friends
+
+var friends = [
+    {
+        "name": "Sunny",
+        "photo": "https://cdn.pixabay.com/photo/2017/12/12/08/11/fruit-3014107_960_720.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Professor Atticus Darnell",
+        "photo": "https://pixabay.com/en/apple-red-apple-red-chief-red-1702316/",
+        "scores":[
+            3,
+            2,
+            3,
+            4,
+            5,
+            2,
+            2,
+            2,
+            4,
+            3
+        ]
+    },
+    {
+        "name": "Murmen",
+        "photo": "https://cdn.pixabay.com/photo/2016/09/06/02/46/eery-1648250_960_720.jpg",
+        "scores":[
+            5,
+            4,
+            5,
+            5,
+            4,
+            5,
+            4,
+            5,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Reggie",
+        "photo": "https://cdn.pixabay.com/photo/2017/10/28/12/57/spirit-2896964_960_720.jpg",
+        "scores":[
+            2,
+            3,
+            1,
+            4,
+            3,
+            2,
+            2,
+            4,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "Senator Reptoid",
+        "photo": "https://cdn.pixabay.com/photo/2017/02/05/11/43/iguana-2039719_960_720.jpg",
+        "scores":[
+            4,
+            5,
+            4,
+            4,
+            1,
+            4,
+            4,
+            3,
+            4,
+            4
+        ]
+    },
+    {
+        "name": "Ms Clock",
+        "photo": "https://cdn.pixabay.com/photo/2017/08/09/13/35/model-2614569_960_720.jpg",
+        "scores":[
+            3,
+            3,
+            5,
+            3,
+            2,
+            3,
+            3,
+            5,
+            3,
+            1
+        ]
+    },
+]
+module.exports = friends;
